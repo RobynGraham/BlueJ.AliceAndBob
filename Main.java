203cc1e2c77d77131ba9eeb33ed90385110123fd
Robyn Graham <robyngraham@zipcoders-MacBook-Pro.local>
2019-02-05 18:42:24 -0500

    Finished Bob and Alice

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +6,15 @@ public class Main {
 
     public static void main(String[] args ){
 
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is your name?");
+        String name = in.nextLine();
+
+        if(name.equals("Alice")||name.equals("Bob")){
+            System.out.println("Hello "+name);
+
+      
+        }
     }
 }
